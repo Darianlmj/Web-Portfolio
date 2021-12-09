3ee230a188fb4fda70e4dd3105464262fc82e400
Darian Lee <darianlmj@gmail.com>
2021-12-09 19:26:02 +1100

    Feature: added 404 page

---
+++ src/pages/404NotFound/PageNotFoundElements.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NotFoundTitle = styled.div`
+    font-size: 4rem;
+    font-weight: 700;
+    font-family: 'lyon-display-web';
+    text-align: center;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    max-width: calc(1200px - (50px * 2));
+    margin: 0 auto;
+`
