ae0a68cba772b0e8482f5f259ce3b77a3e3f12ee
Darian Lee <darianlmj@gmail.com>
2021-12-17 03:44:09 +1100

    Fixed styling for Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,26 +3,22 @@ import './index.css'
 import { IntroAnimation, IntroAnimation2 } from "../../components/Animation/IntroAnimation"
 import Planet2 from "../../images/Planet2.png"
 
-const TestHome = () => {
+const Home = () => {
     return (        
         <main id="site-main" className="site-main-outer">
             <div className="inner">
                 <img className="planet responsive" 
-                src={Planet2}
-                alt="flag{C0nGRaT5! y0U F0uNd M3 =)}"/>
+                     src={Planet2}
+                     alt="flag{C0nGRaT5! y0U F0uNd M3 =)}"/>
                 <div className="intro">
-                    <h1 className="intro-title">
-                                    DARIAN <br></br> LEE
-                    </h1>
+                    <h1 className="intro-title">DARIAN <br></br> LEE</h1>
                     <section className="intro-excerpt">
-                        <IntroAnimation></IntroAnimation>
+                        <IntroAnimation/>
                     </section>
                     <h2 className="intro-2">
-                        <IntroAnimation2></IntroAnimation2>
+                        <IntroAnimation2/>
                     </h2>
                 </div>
-
-
             </div>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
@@ -31,4 +27,4 @@ const TestHome = () => {
     )
 }
 
-export default TestHome
+export default Home
