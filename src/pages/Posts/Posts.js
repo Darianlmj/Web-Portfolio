638d9a7acc265d76640200628fa197abc2a843d4
Darian Lee <darianlmj@gmail.com>
2021-12-17 14:25:16 +1100

    Changed background colour for consistency

--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -13,34 +13,35 @@ const Posts = () => {
     const posts = getPosts();
 
     return (
-        <Container maxWidth="md" sx={{paddingTop: 5}}>
-            <Typography sx={{fontSize: 80, fontFamily: 'lyon-display-web', fontWeight: 800}}>
-                Posts &nbsp;
-                <Emoji  symbol="📬"/>
-            </Typography>
-            <Typography sx={{color: '#777777'}}>
-                This page contains insight into some of the projects I've worked on. Typically, 
-                these projects take a considerable amount of time to make so the frequency of which posts 
-                are churned out may not be very frequent. However, I aim to write up at least one post a month.
-                Hope you understand.
-            </Typography>
+        <div style={{backgroundColor:'#fffdfa', paddingBottom: 10}}>
+            <Container maxWidth="md" sx={{paddingTop: 5}}>
+                <Typography sx={{fontSize: 80, fontFamily: 'lyon-display-web', fontWeight: 800}}>
+                    Posts &nbsp;
+                    <Emoji  symbol="📬"/>
+                </Typography>
+                <Typography sx={{color: '#777777'}}>
+                    This page contains insight into some of the projects I've worked on. Typically, 
+                    these projects take a considerable amount of time to make so the frequency of which posts 
+                    are churned out may not be very frequent. However, I aim to write up at least one post a month.
+                    Hope you understand.
+                </Typography>
 
-            <Grid container spacing={2} sx={{justifyContent: 'space-evenly', marginTop: 6, marginBottom: 6}}>
-                {posts.map((post) => (
-                    <Grid  xs={5}>
-                        <Link to={`/post/${post.id}`} style={{ textDecoration: 'none' }}>
-                            <Card className="postcard" sx={{padding: 3, borderRadius: 3, minHeight: 290}}>
-                                <Typography sx={{fontSize: '1.1rem', color: '#4285F4'}}>{post.tag}</Typography>
-                                <Typography sx={{fontSize: '2rem'}}>{post.title}</Typography>
-                                <Divider sx={{paddingTop: 1, marginBottom: 1.3}} />
-                                <Typography variant='p' sx={{color: '#777777'}}>{post.description}</Typography>
-                            </Card>
-                        </Link>
-                    </Grid>
-                ))}
-            </Grid>
-
-        </Container>
+                <Grid container spacing={2} sx={{justifyContent: 'space-evenly', marginTop: 6, marginBottom: 6}}>
+                    {posts.map((post) => (
+                        <Grid  xs={5}>
+                            <Link to={`/post/${post.id}`} style={{ textDecoration: 'none' }}>
+                                <Card className="postcard" sx={{padding: 3, borderRadius: 3, minHeight: 290}}>
+                                    <Typography sx={{fontSize: '1.1rem', color: '#4285F4'}}>{post.tag}</Typography>
+                                    <Typography sx={{fontSize: '2rem'}}>{post.title}</Typography>
+                                    <Divider sx={{paddingTop: 1, marginBottom: 1.3}} />
+                                    <Typography variant='p' sx={{color: '#777777'}}>{post.description}</Typography>
+                                </Card>
+                            </Link>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </div>
     )
 }
 
